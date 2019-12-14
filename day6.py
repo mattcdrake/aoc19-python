1cf2365074d278b455fda0186c1d6b62f1a48937
Matt Drake <drake.mattc@gmail.com>
2019-12-14 16:16:39 -0500

    Added sorting for day 6

---
+++ day6.py
@@ -0,0 +1,59 @@
+class OrbitPair:
+    def __init__(self, orbitee, orbiter):
+        self.orbitee = orbitee
+        self.orbiter = orbiter
+
+
+class OrbitNode:
+    def __init__(self, id, parent, child):
+        self.id = id
+        self.parent = parent
+        self.children = []
+        if child is not None:
+            self.children.append(child)
+
+
+def prep_input(path):
+    with open(path) as f:
+        orbits_raw = f.readlines()
+    orbits = []
+    for orbit in orbits_raw:
+        pair = orbit.rstrip().split(")")
+        pair = OrbitPair(pair[0], pair[1])
+        orbits.append(pair)
+    return orbits
+
+
+def sortOrbits(orbits):
+    if len(orbits) <= 1:
+        return
+
+    # Put "COM" first
+    for i in range(0, len(orbits)):
+        if orbits[i].orbitee == "COM":
+            temp = orbits[0]
+            orbits[0] = orbits[i]
+            orbits[i] = temp
+
+    # Sort the rest of the orbit pairs
+    trail, inner, lead = (1, 1, 1)
+    target = orbits[0].orbiter
+    while trail < len(orbits):
+        while lead < len(orbits):
+            if orbits[lead].orbitee == target:
+                temp = orbits[inner]
+                orbits[inner] = orbits[lead]
+                orbits[lead] = temp
+                inner += 1
+            lead += 1
+        target = orbits[trail].orbiter
+        trail += 1
+        lead = inner
+    return
+
+
+orbits = prep_input("./input/day6.txt")
+sortOrbits(orbits)
+
+for i in range(0, len(orbits)):
+    print(orbits[i].orbitee + " ) " + orbits[i].orbiter)
