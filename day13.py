5f9cad612aece9674444db4c4e969e8dea4212d0
Matt Drake <drake.mattc@gmail.com>
2019-12-29 23:36:22 -0500

    Finish day 13 part 2

--- day13.py
+++ day13.py
@@ -1,5 +1,6 @@
 from intcode import IntcodeComputer
 from helpers import prep_intcode_program
+from fractions import Fraction
 
 
 def check_bounds(screen, x, y):
@@ -26,11 +27,24 @@ def count_tiles(screen, tile):
     return total
 
 
-def print_screen(screen):
-    for row in screen:
-        for col in row:
-            print(col, end="")
-        print()
+def find_tile(screen, tile):
+    x = -1
+    y = -1
+    for index_y, row in enumerate(screen):
+        for index_x, col in enumerate(row):
+            if col == tile:
+                x = index_x
+                y = index_y
+    return (x, y)
+
+
+def update_joystick(ball, paddle):
+    if paddle[0] > ball[0]:
+        return -1
+    elif paddle[0] < ball[0]:
+        return 1
+    else:
+        return 0
 
 
 program = prep_intcode_program("./input/day13_1.txt")
@@ -44,5 +58,27 @@ while output[1] != "halt":
     output = computer.compute([])
     place_tile(screen, x, y, output[0])
 
-print_screen(screen)
 print("part 1: " + str(count_tiles(screen, 2)))
+
+program = prep_intcode_program("./input/day13_2.txt")
+computer = IntcodeComputer(program.copy())
+screen = [[0]]
+score = 0
+joystick = 0
+output = ("dummy", "dummy")
+while output[1] != "halt":
+    x = computer.compute([joystick])[0]
+    y = computer.compute([joystick])[0]
+    output = computer.compute([joystick])
+    if x == -1 and y == 0:
+        score = output[0]
+    else:
+        check_bounds(screen, x, y)
+        place_tile(screen, x, y, output[0])
+
+    ball = find_tile(screen, 4)
+    paddle = find_tile(screen, 3)
+    if ball[0] != -1 and paddle[0] != -1:
+        joystick = update_joystick(ball, paddle)
+
+print("part 2: " + str(score))
